f8fd424adcb6affee265db952346520a987d964f
The Podz <stefanpodz@yahoo.com>
2021-04-17 16:31:33 -0400

    mysql tables created and initial prompt created

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const inquirer = require('inquirer');
 require('console.table');
 const connection = require('./db/connection');
 
-function viewAllEmployees() {
-    connection.query(
-        'SELECT * FROM employees', (err, results) => {
-            if (err) throw err;
-            
-    )
-}
+
 
 function promptQuestions() {
 
@@ -17,15 +11,34 @@ function promptQuestions() {
             type: 'list',
             name: 'choice',
             message: "What would you like to do?",
-            choices: ['View All Employees', 'View Employees by Department', 'View Employees by Role', "Adde Department", 'Add Employee Role', 'Add Employee', "Update Employee Role"],
+            choices: [
+                'View All Employees',
+                'View Departments',
+                'View Roles',
+                "Add Department",
+                'Add Employee Role',
+                'Add Employee',
+                "Update Employee Role"],
         }
     ])
-    .then(answers => {
-        switch (answers.choice) {
-            case 'View All Employees':
-                
-        }
-    })
+        .then(answers => {
+            switch (answers.choice) {
+                case 'View All Employees':
+                    viewAllEmployees();
+                break;
+
+
+            }
+        })
 }
 
- 
+function viewAllEmployees() {
+    connection.query(
+        'SELECT * FROM employees', (err, results) => {
+            if (err) throw err;
+            console.table(results);
+            promptQuestions();
+
+        })
+}
+promptQuestions();
