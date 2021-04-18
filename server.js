e923ff267055550c9aeb741c2227e273af6c1e65
The Podz <stefanpodz@yahoo.com>
2021-04-17 20:52:50 -0400

    added add dpet and add role functions

--- server.js
+++ server.js
@@ -35,12 +35,22 @@ function promptQuestions() {
                     viewAllRoles();
                 break;
 
+                case 'Add a Department':
+                    addDepartment();
+                break;
+
+                case 'Add an Employee Role':
+                    addEmployeeRole();
+                break;
+
 
 
 
             }
         })
 }
+
+
 
 function viewAllEmployees() {
     connection.query(
@@ -71,5 +81,81 @@ function viewAllRoles() {
 
         })
 }
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: 'newDepartment', 
+                type: 'input', 
+                message: 'Which department would you like to add?'
+            }
+            ]).then(function (answer) {
+                connection.query(
+                    'INSERT INTO departments SET ?',
+                    {
+                        name: answer.newDepartment
+                    });
+                var query = 'SELECT * FROM departments';
+                connection.query(query, function(err, res) {
+                if(err)throw err;
+                console.log('Your department has been added!');
+                console.table('All Departments:', res);
+                promptQuestions();
+                })
+            })
+};
+
+function addEmployeeRole() {
+    connection.query('SELECT * FROM departments', function(err, res) {
+        if (err) throw err;
+    
+        inquirer 
+        .prompt([
+            {
+                name: 'new_role',
+                type: 'input', 
+                message: "What new role would you like to add?"
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of this role? (Enter a number)'
+            },
+            {
+                name: 'Department',
+                type: 'list',
+                choices: function() {
+                    var deptArry = [];
+                    for (let i = 0; i < res.length; i++) {
+                    deptArry.push(res[i].name);
+                    }
+                    return deptArry;
+                },
+            }
+        ]).then(function (answer) {
+            let department_id;
+            for (let a = 0; a < res.length; a++) {
+                if (res[a].name == answer.Department) {
+                    department_id = res[a].id;
+                }
+            }
+    
+            connection.query(
+                'INSERT INTO roles SET ?',
+                {
+                    title: answer.new_role,
+                    salary: answer.salary,
+                    department_id: department_id
+                },
+                function (err, res) {
+                    if(err)throw err;
+                    console.log('Your new role has been added!');
+                    console.table('All Roles:', res);
+                    promptQuestions();
+                })
+        })
+    })
+};
 
 promptQuestions();
